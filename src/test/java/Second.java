564435b2070f44e8296f2b473a87151c3b40fab0
Daniils Zimovs <daniil.zimov@gmail.com>
2021-10-27 21:08:34 +0300

    Initial commit

---
+++ src/test/java/Second.java
@@ -0,0 +1,9 @@
+import basics.MyFirstJavaClass;
+
+public class Second {
+    private void test() {
+      MyFirstJavaClass FirstClass = new MyFirstJavaClass();
+//      Object firstClass;
+//        firstClass.sumTwoDigits(2, 5);
+    }
+}
